4ba80aff1d034032c82d87b4587cce2cc6aa3ec8
Sabine Eitel <Seit201@aol.com>
2022-11-29 10:18:39 +0100

    v1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,15 +7,15 @@ public class App {
 	public static void main(String[] args) {
 		
 		// Vorüberlegung
-		// Cat cat = new Cat("Grizabella");
-		// // output(cat.getAttributes());
+		Cat cat = new Cat("Grizabella");
+		output(cat.getAttributes());
 		
 		//cat1 / cat2 .. more of the same ...DRY
-		// for (int i = 0; i < 10; i++) 
-		// {
-		// 	new Cat("someone_" + i); // Objekt, aber KEINE Referenz!
-		// }
-		
+		for (int i = 0; i < 10; i++) 
+		{
+			new Cat("someone_" + i); // Objekt, aber KEINE Referenz!
+		}
+		// 
 		String [] nameArr = {"Grizabella","Alonzo", "RumTumTugger","Suzanne"};
 		ArrayList<Cat> objList = new ArrayList<>();
 
@@ -30,10 +30,10 @@ public class App {
 
 		}
 
-		output("Cats: " + objList);
-		output(objList.get(0).getAttributes());
-		output(objList.get(1).getAttributes());
-		output(objList.get(objList.size()-1).getAttributes());
+	// 	output("Cats: " + objList);
+	// 	output(objList.get(0).getAttributes());
+	// 	output(objList.get(1).getAttributes());
+	// 	output(objList.get(objList.size()-1).getAttributes());
 		
 
 	}
